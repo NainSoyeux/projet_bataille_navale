c1af1f22a2702455f6551516ff6b2fd11e85cf4b
Raphael GABEZ <raph.gabez@gmail.com>
2025-09-24 09:36:44 +0200

    :tada: Création fichiers scripts

---
+++ story_grille.py
@@ -0,0 +1,10 @@
+"""Nom : "Plouf dans l'eau"
+Utilisateur : un joueur
+Story : On veut pouvoir gérer les tirs de l'adversaire
+Actions :
+créer une grille à 5 lignes et 8 colonnes
+afficher la grille à l'écran
+demande à l'utilisateur de rentrer deux coordonnées x et y
+tier à l'endroit indiqué sur la grille
+retour en 2"""
+
